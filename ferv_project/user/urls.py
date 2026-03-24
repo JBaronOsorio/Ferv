0d00bc1abaeedf77386c70992971d9ea1475da5d
Salome-Serna-R <124018168+Salome-Serna-R@users.noreply.github.com>
2026-03-24 13:10:14 -0500

    fix: code error on html

--- ferv_project/user/urls.py
+++ ferv_project/user/urls.py
@@ -6,6 +6,6 @@ app_name = 'user'
 urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('profile-setup/', views.profile_setup_view, name='profile_setup'),
-    path('login/', views.login_view, name='login'),
+    path('', views.login_view, name='login'), # Set the login view as the default path
     path('logout/', views.logout_view, name='logout'),
 ]
